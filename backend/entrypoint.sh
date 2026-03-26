4552a4269bbc3a808e43023b5f6ebc1526d8bbaa
nduje <dujerancic@gmail.com>
2026-03-26 19:38:18 +0100

    Initialize database schema with prisma

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,8 +4,11 @@ set -e
 echo "Waiting for the database to be ready..."
 sleep 3
 
-echo "Running database migrations..."
-npx prisma migrate deploy
+echo "Applying database structure..."
+npx prisma db push --accept-data-loss
+
+echo "Seeding the database..."
+npx prisma db seed
 
 echo "Starting the application..."
 node dist/src/main.js
